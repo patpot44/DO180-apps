93e7c0833b4f90eacc39bcf1ac91a92891bbff38
Student User <student@workstation.lab.example.com>
2022-10-02 17:25:46 -0400

    to regularize

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+<<<<<<< HEAD
+<<<<<<< HEAD
+print "A change is a coming !!!\n";
+=======
+print "changing"\n";
+>>>>>>> ed02f66c78a6841f29d76a1c15ea3cbab9c11432
+=======
+print "changing"\n";
+>>>>>>> ed02f66c78a6841f29d76a1c15ea3cbab9c11432
+?>
